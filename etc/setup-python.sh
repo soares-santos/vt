9dc0cfa0400e364555623adc7f387058f3ba11ea
Marcelle Soares Santos <marcelle@fnal.gov>
2017-07-09 23:16:59 -0500

    Add etc files in prep for pipeline development

---
+++ etc/setup-python.sh
@@ -0,0 +1,9 @@
+export CONDA_DIR=/cvmfs/des.opensciencegrid.org/fnal/anaconda2/
+export PATH=$CONDA_DIR/bin:$PATH
+source activate $CONDA_DIR/envs/default
+
+export PYTHONPATH=/home/s1/bwelch/new_sklearn/:/home/s1/bwelch/esutil/lib/python2.7/site-packages:$PYTHONPATH
+
+export PYTHONPATH=/data/des41.a/data/marcelle/clusters/colossus:$PYTHONPATH
+
+
